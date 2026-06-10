f2181b9684a7b53f8804dfc8f25972e99207127e
biaodaxuetang <mumu66895@gmail.com>
2026-06-10 21:13:10 +0800

    Update script.js

--- day3/js/script.js
+++ day3/js/script.js
@@ -2,19 +2,19 @@ const courses = [
   {
     title: "说服高手实战营",
     text: "提升沟通效率，化解冲突，建立良好人际关系。",
-    image: "images/shuofu-detail.jpg",
+    image: "images/shuofu-detail.webp",
     buy: "https://s.bluedothub.cn/VM5bxbV"
   },
   {
     title: "表达高手实战营",
     text: "克服紧张，自信开口，掌握演讲逻辑与感染力，让你的汇报更有价值。",
-    image: "images/biaoda-detail.jpg",
+    image: "images/biaoda-detail.webp",
     buy: "https://s.bluedothub.cn/Vd4vapV"
   },
   {
     title: "黄执中说服表达系列课",
     text: "结构化汇报，突出重点，让你的表达更有说服力。",
-    image: "images/huang-detail.jpg",
+    image: "images/huang-detail.webp",
     buy: "https://example.com/buy-report"
   }
 ];
